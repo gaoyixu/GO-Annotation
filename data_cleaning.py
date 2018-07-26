657fdb9c0dbfbfa1459ed4637935eccef36b1d81
gaoyixu <985627758@qq.com>
2018-07-26 19:38:44 +0800

    Update cleaned data

--- data_cleaning.py
+++ data_cleaning.py
@@ -17,17 +17,39 @@ def cleaning(src_path, dst_path):
                 word_list = []
                 word = ''
                 for c in s:
-                    if c.isalpha() or c.isdigit():
-                        word += c  # .lower()
-                    elif word:
-                        word_list.append(word)
-                        word = ''
-                    elif c in letter_set:
-                        word_list.append(c)
+                    if c.isalpha() or c.isdigit() or c == '-':
+                        word += c.lower()
                     else:
-                        pass
+                        if word:
+                            word_list.append(word)
+                            word = ''
+                        if c in letter_set:
+                            word_list.append(c)
                 item_cleaned_list.append(' '.join(word_list))
             fd.writelines('\t'.join(item_cleaned_list) + '\n')
 
 
-cleaning('term_name_def_descriptions_human.txt', 'data_clean.txt')
+def clean_dict(src_path, dst_path):
+    with open(src_path) as fd:
+        lines = fd.readlines()
+    letter_set = {'>', '_', '<', '*', '%', '=', '.', '&', '/',
+                  '[', ']', '+', ')', '~', ':', '(', ';', ',', "'", '\\'}
+    with open(dst_path, 'w') as fd:
+        for line in lines:
+            items = line.split('\t')
+            word_list = []
+            word = ''
+            for c in items[1]:
+                if c.isalpha() or c.isdigit() or c == '-':
+                    word += c.lower()
+                else:
+                    if word:
+                        word_list.append(word)
+                        word = ''
+                    if c in letter_set:
+                        word_list.append(c)
+            fd.write(str(items[0]) + '\t' + ' '.join(word_list) + '\n')
+
+
+clean_dict('gene_dict.txt', 'gene_dict_clean_lower.txt')
+# cleaning('term_name_def_descriptions_human.txt', 'data_clean_lower.txt')
