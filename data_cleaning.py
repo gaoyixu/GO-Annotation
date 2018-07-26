bf4592d4f05ae57f27555431fa62d235e636988a
gaoyixu <985627758@qq.com>
2018-07-26 20:08:23 +0800

    Update dataset

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 def cleaning(src_path, dst_path):
     with open(src_path) as fd:
         lines = fd.readlines()
-    letter_set = {'>', '_', '<', '-', '*', '%', '=', '.', '&', '/',
+    letter_set = {'>', '_', '<', '*', '%', '=', '.', '&', '/',
                   '[', ']', '+', ')', '~', ':', '(', ';', ',', "'", '\\'}
     with open(dst_path, 'w') as fd:
         for line in lines:
@@ -12,7 +12,6 @@ def cleaning(src_path, dst_path):
             items.pop(0)
             descriptions = items
             item_cleaned_list = []
-            word_list = ''
             for s in [term_name, term_def] + descriptions:
                 word_list = []
                 word = ''
@@ -37,19 +36,24 @@ def clean_dict(src_path, dst_path):
     with open(dst_path, 'w') as fd:
         for line in lines:
             items = line.split('\t')
-            word_list = []
-            word = ''
-            for c in items[1]:
-                if c.isalpha() or c.isdigit() or c == '-':
-                    word += c.lower()
-                else:
-                    if word:
-                        word_list.append(word)
-                        word = ''
-                    if c in letter_set:
-                        word_list.append(c)
-            fd.write(str(items[0]) + '\t' + ' '.join(word_list) + '\n')
+            item_cleaned_list = []
+            for s in items[1:]:
+                word_list = []
+                word = ''
+                for c in s:
+                    if c.isalpha() or c.isdigit() or c == '-':
+                        word += c  # .lower()
+                    else:
+                        if word:
+                            word_list.append(word)
+                            word = ''
+                        if c in letter_set:
+                            word_list.append(c)
+                if word:
+                    word_list.append(word)
+                item_cleaned_list.append(' '.join(word_list))
+            fd.writelines(items[0] + '\t' + '\t'.join(item_cleaned_list) + '\n')
 
 
-clean_dict('gene_dict.txt', 'gene_dict_clean_lower.txt')
+clean_dict('go_dict.txt', 'go_dict_clean.txt')
 # cleaning('term_name_def_descriptions_human.txt', 'data_clean_lower.txt')
---
+++ readme.txt
@@ -0,0 +1,8 @@
+gene_dict.txt
+	GeneID + tab + Gene Description
+
+go_dict.txt
+	GO (term ID) + tab + term name + term def
+
+id_data.txt
+	GO (term ID) + tab + UniprotKB:GeneID + tab + …
