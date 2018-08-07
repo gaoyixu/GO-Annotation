7065578db3675d088bdbf8cbb1abdeea7c24a154
gaoyixu <985627758@qq.com>
2018-08-07 14:34:41 +0800

    Update

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,19 @@
-"""
+"""Cleaning data.
+Separate symbols, such as dots and commas.
+
+Author:
+    Yixu Gao
+    gaoyixu1996@outlook.com
+
 Usage:
     cleaning('data/term_name_def_descriptions_human.txt',
          'data/data_clean_lower.txt', True)
     cleaning('data/term_name_def_descriptions_human.txt',
          'data/data_clean.txt', False)
-    clean_dict('data/go_dict.txt', 'data/go_dict_clean.txt')
+    cleaning('data/go_dict.txt', 'data/go_dict_clean_lower.txt', True)
+    cleaning('data/go_dict.txt', 'data/go_dict_clean.txt', False)
+    cleaning('data/gene_dict.txt', 'data/gene_dict_clean_lower.txt', True)
+    cleaning('data/gene_dict.txt', 'data/gene_dict_clean.txt', False)
 """
 
 
@@ -36,33 +45,3 @@ def cleaning(src_path, dst_path, lower_mode=True):
                     word_list.append(word)
                 item_cleaned_list.append(' '.join(word_list))
             fd.writelines('\t'.join(item_cleaned_list) + '\n')
-
-
-def clean_dict(src_path, dst_path):
-    with open(src_path) as fd:
-        lines = fd.readlines()
-    letter_set = {'>', '_', '<', '*', '%', '=', '.', '&', '/',
-                  '[', ']', '+', ')', '~', ':', '(', ';', ',', "'", '\\'}
-    with open(dst_path, 'w') as fd:
-        for line in lines:
-            items = line.split('\t')
-            item_cleaned_list = []
-            for s in items[1:]:
-                word_list = []
-                word = ''
-                for c in s:
-                    if c.isalpha() or c.isdigit() or c == '-':
-                        word += c  # .lower()
-                    else:
-                        if word:
-                            word_list.append(word)
-                            word = ''
-                        if c in letter_set:
-                            word_list.append(c)
-                if word:
-                    word_list.append(word)
-                item_cleaned_list.append(' '.join(word_list))
-            fd.writelines(items[0] + '\t' + '\t'.join(item_cleaned_list) + '\n')
-
-
-clean_dict('data/go_dict.txt', 'data/go_dict_clean.txt')
