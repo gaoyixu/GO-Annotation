22fbeb217600f26386f1165b39a0752d94f6c37e
gaoyixu <985627758@qq.com>
2018-08-29 14:33:00 +0800

    Update

--- load_data.py
+++ load_data.py
@@ -5,15 +5,30 @@ Author:
     gaoyixu1996@outlook.com
 
 Usage:
-    ds = DataSet(128, 'data/word_embedding.txt')
-    train_dataset, test_dataset = ds.load_dict_data(
-        'data/gene_dict_clean_lower.txt')
+    word_vocab_list, embedding_matrix = (
+        load_data.load_word_embedding_for_dict_file(
+            'data/gene_dict_clean_lower.txt',
+            'data/word_embedding.txt'))
+    index_table = tf.contrib.lookup.index_table_from_tensor(
+        word_vocab_list,
+        num_oov_buckets=1,
+        default_value=-1)
+    total_words_num = len(word_vocab_list)
+    ds = load_data.DataSet(
+        'data/gene_dict_clean_lower.txt', self.max_length)
+    train_dataset, test_dataset = ds.load_dict_data()
     iterator = full_dataset.make_initializable_iterator()
     with tf.Session() as sess:
         sess.run(iterator.initializer)
         sess.run(tf.global_variables_initializer())
         sess.run(tf.tables_initializer())
-        print(sess.run(iterator.get_next()))
+        current_str = iterator.get_next()
+        current = index_table.lookup(current_str)
+        print(sess.run(current_str))
+        print(sess.run(current))
+        print(sess.run(tf.nn.embedding_lookup(
+            embedding_matrix, current)))
+        print(sess.run(tf.one_hot(current, total_words_num)))
 """
 
 
@@ -28,127 +43,91 @@ UNKNOWN_WORD = 'UNK'
 SIGN_WORDS = ['PAD', '<s>', '<\s>']
 
 
+def load_word_embedding(vocab, word_embedding_path, sign_words=None):
+    """Load word embedding.
+
+    Args:
+        vocab: string set, vocab set of the dataset
+        word_embedding_path: string, word embedding file path,
+            e.g. glove.6B.200d.txt
+        sign_words: ['PAD', '<s>', '<\s>']
+
+    Returns:
+        word_vocab_list: ['PAD', 'UNK', ...]
+        embedding_matrix: 2-D matrix with vocab_size rows and dim columns
+    """
+    if sign_words is None:
+        sign_words = ['PAD', '<s>', '<\s>']
+
+    word_vocab_list = []
+    embedding_matrix = np.array([], dtype=np.float32)
+
+    try:
+        with open(word_embedding_path) as fd:
+            lines = fd.readlines()
+            word_dimension = len(lines[0].split()) - 1
+
+            word_vocab_list.extend(sign_words)
+
+            for _ in sign_words:
+                embedding_matrix = np.append(
+                    embedding_matrix, np.zeros(word_dimension))
+
+            for line in lines:
+                items = line.split()
+                word = items[0]
+                embedding = np.array(items[1:], dtype=np.float32)
+                if word in vocab:
+                    word_vocab_list.append(word)
+                    embedding_matrix = np.append(
+                        embedding_matrix, embedding)
+
+        # For default unknown word
+        embedding_matrix = np.append(
+            embedding_matrix, np.random.uniform(
+                -1.0, 1.0, word_dimension))
+
+        embedding_matrix = np.reshape(
+            embedding_matrix,
+            [-1, word_dimension]).astype(np.float32)
+
+        return word_vocab_list, embedding_matrix
+
+    except OSError:
+        current_path = os.path.abspath(__file__)
+        abs_file_path = os.path.abspath(os.path.dirname(current_path)
+                                        + os.path.sep + word_embedding_path)
+        print('Could not read embedding file: ' + abs_file_path)
+        exit(0)
+
+
+def load_word_embedding_for_dict_file(file_path, word_embedding_path):
+    """Load word embedding for file.
+
+    Args:
+        file_path: string
+        word_embedding_path: string
+    """
+    vocab = set()
+    with open(file_path) as fd:
+        lines = fd.readlines()
+        for line in lines:
+            words = line.split()[1:]
+            vocab.update(words)
+    return load_word_embedding(vocab, word_embedding_path)
+
+
 class DataSet:
-    def __init__(self,
-                 word_dimension,
-                 word_embedding_path,
-                 sign_words=SIGN_WORDS):
-        self.word_embedding_path = word_embedding_path
-        self.word_dimension = word_dimension
-        self.sign_words = sign_words
-        self.word_vocab_list = []
-        self.embedding_matrix = np.array([], dtype=np.float32)
-        self.table = None
+    def __init__(self, file_path, max_length):
+        """
+        Args:
+            file_path: string, e.g. 'gene_dict_clean_lower.txt'
+            max_length: int
+        """
+        self.file_path = file_path
+        self.max_length = max_length
         self.dataset = None
         self.dataset_size = 0
-
-    def _load_word_embedding(self, vocab, file_path=''):
-        """Load word embedding.
-
-        Args:
-            vocab: string set, vocab set of the dataset
-            file_path: string, word embedding file path,
-                e.g. glove.6B.200d.txt
-
-        Returns:
-            word_vocab_list: ['PAD', 'UNK', ...]
-            embedding_matrix: 2-D matrix with vocab_size rows and dim columns
-        """
-        if not file_path:
-            file_path = self.word_embedding_path
-        else:
-            self.word_embedding_path = file_path
-        try:
-            with open(file_path) as fd:
-                lines = fd.readlines()
-
-                self.word_vocab_list.extend(self.sign_words)
-
-                for _ in self.sign_words:
-                    self.embedding_matrix = np.append(
-                        self.embedding_matrix, np.zeros(self.word_dimension))
-
-                for line in lines:
-                    items = line.split()
-                    word = items[0]
-                    embedding = np.array(items[1:], dtype=np.float32)
-                    if word in vocab:
-                        self.word_vocab_list.append(word)
-                        self.embedding_matrix = np.append(
-                            self.embedding_matrix, embedding)
-
-            # For default unknown word
-            self.embedding_matrix = np.append(
-                self.embedding_matrix, np.random.uniform(
-                    -1.0, 1.0, self.word_dimension))
-
-            self.embedding_matrix = np.reshape(
-                self.embedding_matrix,
-                [-1, self.word_dimension]).astype(np.float32)
-
-            self.table = tf.contrib.lookup.index_table_from_tensor(
-                self.word_vocab_list,
-                num_oov_buckets=1,
-                default_value=-1)
-
-            return self.word_vocab_list, self.embedding_matrix
-
-        except OSError:
-            current_path = os.path.abspath(__file__)
-            abs_file_path = os.path.abspath(os.path.dirname(current_path)
-                                            + os.path.sep + file_path)
-            print('Could not read embedding file: ' + abs_file_path)
-            exit(0)
-
-    def _load_word_embedding_for_file(self, file_path):
-        """Load word embedding for file.
-
-        Args:
-            file_path: string
-        """
-        vocab = set()
-        with open(file_path) as fd:
-            lines = fd.readlines()
-            for line in lines:
-                words = line.split()[1:]
-                vocab.update(words)
-        self._load_word_embedding(vocab)
-
-    def _encode_sentence_to_index(self, words):
-        """Encode the sentence using vocab.
-
-        Args:
-            words: tf.string tensor
-
-        Returns:
-            encoded sentence: 1-D int list of word index
-        """
-        encoded_sentence = self.table.lookup(words)
-        return encoded_sentence
-
-    def _encode_indexed_sentence_to_embedding(self, indexed_sentence):
-        """Encode the sentence using vocab.
-
-        Args:
-            indexed_sentence: 1-D int list of word index
-
-        Returns:
-            encoded sentence: 2-D tensor of word embedding
-        """
-        return tf.nn.embedding_lookup(
-            self.embedding_matrix, indexed_sentence)
-
-    def _encode_sentence_to_embedding(self, sentence):
-        """Encode the sentence using vocab.
-
-        Args:
-            sentence: string
-
-        Returns:
-            encoded sentence: 2-D tensor of word embedding
-        """
-        indexed_sentence = self._encode_sentence_to_index(sentence)
-        return self._encode_indexed_sentence_to_embedding(indexed_sentence)
 
     def _split_train_test(self,
                           batch_size,
@@ -167,41 +146,33 @@ class DataSet:
         if self.dataset:
             train_size = int(train_div * self.dataset_size)
             self.dataset = (self.dataset
-                            .shuffle(10000)
                             .padded_batch(batch_size, padded_shapes,
                                           b'PAD'))
             train_dataset = self.dataset.take(train_size)
             test_dataset = self.dataset.skip(train_size)
             return train_dataset, test_dataset
 
-    def load_dict_data(self, file_path):
+    def load_dict_data(self):
         """Load data for gene dict and go dict.
-
-        Args:
-            file_path: string, e.g. 'gene_dict_clean_lower.txt'
 
         Returns:
             tf.data.Dataset for each contains
                 (GeneID, Gene Description (string))
                 e.g. (1, b'alpha-1-b glycoprotein')
         """
-        self._load_word_embedding_for_file(file_path)
-        with open(file_path) as fd:
-            lines = fd.readlines()
-        self.dataset_size = len(lines)
-        del lines
-        self.dataset = tf.data.TextLineDataset(file_path)
-        self.dataset = self.dataset.map(
-            lambda x: tf.sparse_tensor_to_dense(
-                tf.string_split([x], '\t'), '')[0])
-        self.dataset = self.dataset.map(
-            lambda x: (x[0], tf.sparse_tensor_to_dense(
-                tf.string_split([x[1]]), '')[0][:13]))
-        self.dataset = self.dataset.map(
-            lambda x, y: tf.concat(
-                [tf.convert_to_tensor(['<s>'], dtype=tf.string),
-                 y, tf.convert_to_tensor(['<\s>'], dtype=tf.string)], 0))
-        train_dataset, test_dataset = self._split_train_test(2, 15)
-        train_dataset = train_dataset.map(self._encode_sentence_to_embedding)
-        test_dataset = test_dataset.map(self._encode_sentence_to_embedding)
+        with tf.name_scope('dataset'):
+            with open(self.file_path) as fd:
+                lines = fd.readlines()
+            self.dataset_size = len(lines)
+            del lines
+            self.dataset = tf.data.TextLineDataset(self.file_path)
+            self.dataset = (self.dataset.map(
+                lambda x: tf.sparse_tensor_to_dense(
+                    tf.string_split([x], '\t'), '')[0])
+                .map(lambda x: tf.sparse_tensor_to_dense(
+                    tf.string_split([x[1]]), '')[0][:self.max_length - 2])
+                .map(lambda x: tf.concat(
+                    [tf.convert_to_tensor(['<s>'], dtype=tf.string),
+                     x, tf.convert_to_tensor(['<\s>'], dtype=tf.string)], 0)))
+            train_dataset, test_dataset = self._split_train_test(2, self.max_length)
         return train_dataset, test_dataset
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import os
 
 
-from load_data import DataSet
+import load_data
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -11,30 +11,133 @@ NUM_EPOCHS = 1
 
 
 class Model:
-    def __init__(self, hyperparameters=None,
-                 train_set=None,
-                 test_set=None):
-        self.hyperparameters = hyperparameters
-        self.train_set = train_set
-        self.test_set = test_set
-
-    def graph(self):
-
+    def __init__(self,
+                 max_length,
+                 encoder_hidden_sizes,
+                 decoder_hidden_size,
+                 batch_size):
+        self.max_length = max_length
+        self.encoder_hidden_sizes = encoder_hidden_sizes
+        self.decoder_hidden_size = decoder_hidden_size
+        self.batch_size = batch_size
         pass
 
+    def graph(self):
+        tf.get_variable('')
+        pass
+
+    def _encoder(self, batch_data):
+        with tf.name_scope('encoder'):
+            fw_cells = [tf.nn.rnn_cell.DropoutWrapper(
+                tf.nn.rnn_cell.BasicLSTMCell(size))
+                      for size in self.encoder_hidden_sizes]
+            bw_cells = [tf.nn.rnn_cell.DropoutWrapper(
+                tf.nn.rnn_cell.BasicLSTMCell(size))
+                      for size in self.encoder_hidden_sizes]
+            encoder_outputs_all, encoder_state_fw, encoder_state_bw = (
+                tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
+                    fw_cells, bw_cells, batch_data, dtype=tf.float32))
+            encoder_outputs = tf.concat(encoder_outputs_all, 2)
+            encoder_state_c = tf.concat((encoder_state_fw[0].c, encoder_state_bw[0].c), 1)
+            encoder_state_h = tf.concat((encoder_state_fw[0].h, encoder_state_bw[0].h), 1)
+            encoder_state = tf.contrib.rnn.LSTMStateTuple(c=encoder_state_c, h=encoder_state_h)
+        return encoder_outputs, encoder_state
+
+    def _decoder(self, batch_data, encoder_outputs, encoder_state, training_mode=True):
+        with tf.name_scope('decoder'), tf.variable_scope("decoder") as decoder_scope:
+            if training_mode:
+                decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
+                    tf.nn.rnn_cell.BasicLSTMCell(self.decoder_hidden_size * 2),
+                    tf.contrib.seq2seq.BahdanauAttention(
+                        self.decoder_hidden_size * 2,
+                        encoder_outputs,
+                        normalize=True),
+                    attention_layer_size=self.decoder_hidden_size * 2)
+                initial_state = decoder_cell.zero_state(
+                    dtype=tf.float32, batch_size=self.batch_size)
+                initial_state = initial_state.clone(cell_state=encoder_state)
+                helper = tf.contrib.seq2seq.TrainingHelper(batch_data, self.max_length)
+                decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, helper, initial_state)
+                outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder, scope=decoder_scope)
+                self.decoder_output = outputs.rnn_output
+                self.logits = self.projection_layer(self.decoder_output)
+                self.logits_reshape = tf.concat(
+                    [self.logits,
+                     tf.zeros([self.batch_size, summary_max_len - tf.shape(self.logits)[1], self.vocabulary_size])],
+                    axis=1)
+            else:
+                tiled_encoder_output = tf.contrib.seq2seq.tile_batch(
+                    tf.transpose(self.encoder_output, perm=[1, 0, 2]), multiplier=self.beam_width)
+                tiled_encoder_final_state = tf.contrib.seq2seq.tile_batch(self.encoder_state,
+                                                                          multiplier=self.beam_width)
+                tiled_seq_len = tf.contrib.seq2seq.tile_batch(self.X_len, multiplier=self.beam_width)
+                attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+                    self.num_hidden * 2, tiled_encoder_output, memory_sequence_length=tiled_seq_len, normalize=True)
+                decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism,
+                                                                   attention_layer_size=self.num_hidden * 2)
+                initial_state = decoder_cell.zero_state(dtype=tf.float32, batch_size=self.batch_size * self.beam_width)
+                initial_state = initial_state.clone(cell_state=tiled_encoder_final_state)
+                decoder = tf.contrib.seq2seq.BeamSearchDecoder(
+                    cell=decoder_cell,
+                    embedding=self.embeddings,
+                    start_tokens=tf.fill([self.batch_size], tf.constant(2)),
+                    end_token=tf.constant(3),
+                    initial_state=initial_state,
+                    beam_width=self.beam_width,
+                    output_layer=self.projection_layer
+                )
+                outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+                    decoder, output_time_major=True, maximum_iterations=summary_max_len, scope=decoder_scope)
+                self.prediction = tf.transpose(outputs.predicted_ids, perm=[1, 2, 0])
+
+    def _loss(self):
+        with tf.name_scope("loss"):
+            if not forward_only:
+                crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    logits=self.logits_reshape, labels=self.decoder_target)
+                weights = tf.sequence_mask(self.decoder_len, summary_max_len, dtype=tf.float32)
+                self.loss = tf.reduce_sum(crossent * weights / tf.to_float(self.batch_size))
+
+                params = tf.trainable_variables()
+                gradients = tf.gradients(self.loss, params)
+                clipped_gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+                optimizer = tf.train.AdamOptimizer(self.learning_rate)
+                self.update = optimizer.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step)
+
     def train(self):
-        ds = DataSet(128, 'data/word_embedding.txt')
-        train_dataset, test_dataset = ds.load_dict_data(
-            'data/gene_dict_clean_lower.txt')
+        with tf.name_scope('embedding'):
+            word_vocab_list, embedding_matrix = (
+                load_data.load_word_embedding_for_dict_file(
+                    'data/gene_dict_clean_lower.txt',
+                    'data/word_embedding.txt'))
+            index_table = tf.contrib.lookup.index_table_from_tensor(
+                word_vocab_list,
+                num_oov_buckets=1,
+                default_value=-1)
+            total_words_num = len(word_vocab_list)
+
+        with tf.name_scope('dataset'):
+            ds = load_data.DataSet(
+                'data/gene_dict_clean_lower.txt', self.max_length)
+            train_dataset, test_dataset = ds.load_dict_data()
+
         iterator = train_dataset.make_initializable_iterator()
+
         with tf.Session() as sess:
+            tf.summary.FileWriter('graphs/test', sess.graph)
             sess.run(tf.global_variables_initializer())
             sess.run(tf.tables_initializer())
             for i in range(NUM_EPOCHS):
                 sess.run(iterator.initializer)
                 try:
                     # while True:
-                        print(sess.run(iterator.get_next()))
+                        current_str = iterator.get_next()
+                        current = index_table.lookup(current_str)
+                        print(sess.run(current_str))
+                        print(sess.run(current))
+                        print(sess.run(tf.nn.embedding_lookup(
+                            embedding_matrix, current)))
+                        print(sess.run(tf.one_hot(current, total_words_num)))
                 except tf.errors.OutOfRangeError:
                     pass
 
@@ -42,5 +145,5 @@ class Model:
         pass
 
 
-model = Model()
+model = Model(15, [256], 256, 32)
 model.train()
