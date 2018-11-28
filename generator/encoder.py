5962ca07b0fa7724ae69f3710feaa361c9b83cf3
dongfanker <563114215@qq.com>
2018-11-29 00:55:55 +0800

    encoder change

--- generator/encoder.py
+++ generator/encoder.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # coding=UTF-8
-=======
->>>>>>> 88cd809551d2f7302bee3f26c8eeccf66e061530
 import torch.nn as nn
 import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,28 +8,18 @@ class EncoderRNN(nn.Module):
         super(EncoderRNN, self).__init__()
         self.n_layers = n_layers
         self.hidden_size = hidden_size
-<<<<<<< HEAD
         # self.embedding = nn.Embedding(input_size, hidden_size)
-=======
-        self.embedding = nn.Embedding(input_size, hidden_size)
->>>>>>> 88cd809551d2f7302bee3f26c8eeccf66e061530
         self.gru = nn.GRU(
             hidden_size,
             hidden_size
            )
 
-<<<<<<< HEAD
     def forward(self, embedded,hidden):
         # embedded = self.embedding(input_seq).view(1, 1, -1)
         # 残余问题：如何正确地将结果输入进去
         # print("1",embedded.view(1, 1, -1).size())
         # print("2",hidden.size())
         outputs, hidden = self.gru(embedded.view(1, 1, -1), hidden)
-=======
-    def forward(self, input_seq, hidden=None):
-        embedded = self.embedding(input_seq).view(1, 1, -1)
-        outputs, hidden = self.gru(embedded, hidden)
->>>>>>> 88cd809551d2f7302bee3f26c8eeccf66e061530
         return outputs, hidden
 
     def initHidden(self,device = device):
